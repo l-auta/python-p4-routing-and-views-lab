2eb567b77cb420a50ed1701252750f7b14517d24
lisa <lisaauta018@gmail.com>
2025-01-13 19:27:30 +0300

    one is failing

--- server/app.py
+++ server/app.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    
+    return f'{param}'
+
+@app.route('/count/<int:param>')
+def count(param):
+    # Create a list of numbers from 0 to param-1
+    numbers = '\n'.join(str(i) for i in range(param))
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        # Handling division by zero
+        if num2 == 0:
+            return "Error: Division by zero is not allowed.", 400
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Error: Invalid operation.", 400
+    
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
